3f8c63c0224edcf73fa49f47880c5b7c3fbc443f
isahiir <87277968+isahiir@users.noreply.github.com>
2025-05-01 20:12:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ const imagePreview = document.getElementById('imagePreview');
 // --- TÄHTIS ---
 // ASENDA SEE OMA GOOGLE APPS SCRIPT WEB APP URL-IGA!
 const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzxSiFHySk3FM_v-AbO1piDrPIeNyIb-CSdlannU5fRWoKSQIZHDfPyg2zD820jS-k/exec'; // KASUTA SAMA URL-I, MIS ENNE
+
 // ---------------
 
 let currentStream = null; // Hoiab aktiivset kaameravoogu
